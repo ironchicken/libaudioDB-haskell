a1599b848486a2b871932881c29be02af2a779e2
Richard Lewis <richard.lewis@gold.ac.uk>
2014-08-19 17:44:15 +0100

    Added test for sequence query

--- tests/AudioDBTest.hs
+++ tests/AudioDBTest.hs
@@ -21,22 +21,49 @@ test_readCSVFeatures key fp = do
         free p
     ) datumPtr
 
+sample_rate :: Int
+sample_rate = 44100
+
+step_size :: Int
+step_size = 2048
+
+framesPerSecond :: Double
+framesPerSecond = sr / ss
+  where sr = fromIntegral sample_rate
+        ss = fromIntegral step_size
+
+test_query :: FilePath -> FilePath -> FilePath -> Seconds -> Seconds -> IO ()
+test_query adbFile queryFile qPowersFile start len = do
+  adbFN  <- newCString adbFile
+  adb    <- audiodb_open adbFN 0
+  if adb == nullPtr then putStrLn $ "Could not open " ++ (show adbFile) else putStrLn $ "Opened " ++ (show adbFile)
+  queryFeatures <- readCSVFeaturesTimesPowers "chester_16" queryFile qPowersFile
+  maybe (putStrLn $ "Could not parse " ++ queryFile)
+    (\p -> do
+        putStrLn $ "Parsed " ++ queryFile
+        let res = execSequenceQuery adb p (floor . (* framesPerSecond)) 1 10 start len
+        putStrLn (show res)
+    )
+    queryFeatures
+
 main :: IO ()
 main = do
   -- FIXME If you reverse the sequence of insertCSVFeatures and
   -- readCSVFeatures, the insert seems to be ineffective (i.e. audioDB
   -- -Z on the resulting DB shows no content)
-  fp       <- newCString "wagner.adb"
-  adb      <- audiodb_create fp (CUInt 0) (CUInt 0) (CUInt 12)
+  -- fp       <- newCString "wagner.adb"
+  -- adb      <- audiodb_create fp (CUInt 0) (CUInt 0) (CUInt 12)
 
-  datumPtr <- readCSVFeaturesTimes "WandererSceneImplicit" "WandererSceneImplicit_vamp_nnls-chroma_nnls-chroma_chroma.csv"
-  inserted <- insertMaybeFeatures adb datumPtr
-  putStrLn $ "Inserted 'WandererSceneImplicit': " ++ (show inserted)
-  maybe (return ()) (\d -> free d) datumPtr
+  -- datumPtr <- readCSVFeaturesTimes "WandererSceneImplicit" "WandererSceneImplicit_vamp_nnls-chroma_nnls-chroma_chroma.csv"
+  -- inserted <- insertMaybeFeatures adb datumPtr
+  -- putStrLn $ "Inserted 'WandererSceneImplicit': " ++ (show inserted)
+  -- maybe (return ()) (\d -> free d) datumPtr
 
-  features <- featuresFromKey adb "WandererSceneImplicit"
-  maybe (putStrLn "Could not retrieve 'WandererSceneImplicit'") (\f -> do putStrLn $ "Found '" ++ (datum_key f) ++ "'") features
+  -- features <- featuresFromKey adb "WandererSceneImplicit"
+  -- maybe (putStrLn "Could not retrieve 'WandererSceneImplicit'") (\f -> do putStrLn $ "Found '" ++ (datum_key f) ++ "'") features
 
-  test_readCSVFeatures "WandererSceneImplicit" "WandererSceneImplicit_vamp_nnls-chroma_nnls-chroma_chroma.csv"
+  -- -- test_readCSVFeatures "WandererSceneImplicit" "WandererSceneImplicit_vamp_nnls-chroma_nnls-chroma_chroma.csv"
+
+  test_query "parsifal_chroma.adb" "chester_16_chroma.csv" "chester_16_power.csv" 8 11
 
   putStrLn "Done."
