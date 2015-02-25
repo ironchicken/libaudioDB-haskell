30c073fa435bb2686eeed3ded915a55b69141337
Richard Lewis <richard.lewis@gold.ac.uk>
2015-02-25 17:36:41 +0000

    Added tests for query callback and transformation functions

--- tests/AudioDBTest.hs
+++ tests/AudioDBTest.hs
@@ -68,6 +68,70 @@ test_query adbFile queryFile qPowersFile start len = do
     )
     queryFeatures
 
+test_callback_query :: FilePath -> FilePath -> FilePath -> Seconds -> Seconds -> IO ()
+test_callback_query adbFile queryFile qPowersFile start len = do
+  adbFN  <- newCString adbFile
+  adb    <- audiodb_open adbFN 0
+  if adb == nullPtr then putStrLn $ "Could not open " ++ (show adbFile) else putStrLn $ "Opened " ++ (show adbFile)
+  queryFeatures <- readCSVFeaturesTimesPowers "chester_16" queryFile qPowersFile
+  maybe (putStrLn $ "Could not parse " ++ queryFile)
+    (\datumPtr -> do
+        let ntracks        = query_parameters_ntracks . query_spec_params
+            qAlloc         = mkSequenceQuery datumPtr (floor . (* framesPerSecond)) 1 5 start len (Just euclideanNormedFlag) Nothing
+            isFinished _ _ = putStrLn "isFinished..." >> return False -- withQueryPtr adb a (\qPtr -> do { q <- peek qPtr; return $ ntracks q >= 25 })
+            callback r     = do
+              res <- peek r
+              n <- return $ query_results_nresults res
+              putStrLn $ "Callback says: " ++ (show n)
+              putStrLn (showResults res)
+              return n
+
+        putStrLn $ "Parsed " ++ queryFile
+        _ <- queryWithCallback adb qAlloc callback isFinished
+        return ())
+    queryFeatures
+
+test_transform_query :: FilePath -> FilePath -> FilePath -> Seconds -> Seconds -> IO ()
+test_transform_query adbFile queryFile qPowersFile start len = do
+  adbFN  <- newCString adbFile
+  adb    <- audiodb_open adbFN 0
+  if adb == nullPtr then putStrLn $ "Could not open " ++ (show adbFile) else putStrLn $ "Opened " ++ (show adbFile)
+  queryFeatures <- readCSVFeaturesTimesPowers "chester_16" queryFile qPowersFile
+  maybe (putStrLn $ "Could not parse " ++ queryFile)
+    (\datumPtr -> do
+        let ntracks        = query_parameters_ntracks . query_spec_params
+            qAlloc         = mkSequenceQuery datumPtr (floor . (* framesPerSecond)) 1 5 start len (Just euclideanNormedFlag) Nothing
+            isFinished _ r = withResults r (\res -> return $ (query_results_nresults res) >= 20)
+            transform r a  = mkSequenceQueryDeltaNTracks (floor . (* framesPerSecond)) framesToSeconds (\x -> x + 5) r a
+
+        putStrLn $ "Parsed " ++ queryFile
+        res <- queryWithTransform adb qAlloc transform isFinished
+        putStrLn "Final results:"
+        withResults res (\r -> putStrLn (showResults r))
+        return ())
+    queryFeatures
+
+test_callbacktransform_query :: FilePath -> FilePath -> FilePath -> Seconds -> Seconds -> IO ()
+test_callbacktransform_query adbFile queryFile qPowersFile start len = do
+  adbFN  <- newCString adbFile
+  adb    <- audiodb_open adbFN 0
+  if adb == nullPtr then putStrLn $ "Could not open " ++ (show adbFile) else putStrLn $ "Opened " ++ (show adbFile)
+  queryFeatures <- readCSVFeaturesTimesPowers "chester_16" queryFile qPowersFile
+  maybe (putStrLn $ "Could not parse " ++ queryFile)
+    (\datumPtr -> do
+        let ntracks        = query_parameters_ntracks . query_spec_params
+            qAlloc         = mkSequenceQuery datumPtr (floor . (* framesPerSecond)) 1 5 start len (Just euclideanNormedFlag) Nothing
+            isFinished a _ = withQuery adb a (\q -> do { return $ (ntracks q) >= 20 }) --withResults r (\res -> return $ (query_results_nresults res) >= 20)
+            callback r     = withResults r (\res -> do { putStrLn (showResults res); return $ (query_results_nresults res) })
+            transform r a  = mkSequenceQueryDeltaNTracks (floor . (* framesPerSecond)) framesToSeconds (\x -> x + 5) r a
+
+        putStrLn $ "Parsed " ++ queryFile
+        res <- queryWithCallbacksAndTransform adb qAlloc transform callback isFinished
+        putStrLn "Final results:"
+        withResults res (\r -> putStrLn (showResults r))
+        return ())
+    queryFeatures
+
 db_file :: String
 db_file = undefined
 
@@ -79,6 +143,12 @@ test_features_file = undefined
 
 test_power_features_file :: String
 test_power_features_file = undefined
+
+query_seq_start :: Seconds
+query_seq_start = undefined
+
+query_seq_length :: Seconds
+query_seq_length = undefined
 
 main :: IO ()
 main = do
@@ -99,5 +169,7 @@ main = do
   -- test_readCSVFeatures test_features_name test_features_file
 
   -- test_query db_file test_features_file test_power_features_file query_seq_start query_seq_length
+  -- test_transform_query db_file test_features_file test_power_features_file query_seq_start query_seq_length
+  -- test_callbacktransform_query db_file test_features_file test_power_features_file query_seq_start query_seq_length
 
   putStrLn "Done."
